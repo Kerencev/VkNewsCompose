7a42f2061915a7e3a6a90899fad189aa0257d97a
Sergei Kerencev <SYKerentsev@sberbank.ru>
2023-03-22 14:20:00 +0300

    Added a news card

--- app/src/main/java/com/kerencev/vknewscompose/MainActivity.kt
+++ app/src/main/java/com/kerencev/vknewscompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kerencev.vknewscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -16,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +29,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsComposeTheme {
-                NewsCard()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                ) {
+                    NewsCard()
+                }
             }
         }
     }
@@ -35,45 +43,94 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsCard() {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Row(
+    Card {
+        Column(
             modifier = Modifier
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(8.dp)
         ) {
+            NewsHeader()
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = stringResource(id = R.string.temp_text))
+            Spacer(modifier = Modifier.height(4.dp))
             Image(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .size(50.dp),
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = stringResource(
-                    id = R.string.group_avatar
-                )
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.post_content_image),
+                contentDescription = stringResource(id = R.string.post_content_image),
+                contentScale = ContentScale.FillWidth
             )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 4.dp)
-            ) {
-                Text(
-                    text = "Название группы",
-                    color = MaterialTheme.colors.onPrimary
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "14:00",
-                    color = MaterialTheme.colors.onSecondary
-                )
-            }
-            Icon(
-                imageVector = Icons.Filled.MoreVert,
-                contentDescription = stringResource(id = R.string.more),
-                tint = MaterialTheme.colors.onSecondary
+            Spacer(modifier = Modifier.height(8.dp))
+            NewsFooter()
+        }
+    }
+}
+
+@Composable
+fun NewsHeader() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(50.dp),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = stringResource(
+                id = R.string.group_avatar
+            )
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+        ) {
+            Text(
+                text = "Название группы",
+                color = MaterialTheme.colors.onPrimary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colors.onSecondary
             )
         }
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = stringResource(id = R.string.more),
+            tint = MaterialTheme.colors.onSecondary
+        )
+    }
+}
+
+@Composable
+fun NewsFooter() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(modifier = Modifier.weight(1f)) {
+            IconWithText(iconRes = R.drawable.ic_views_count, text = "976")
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(iconRes = R.drawable.ic_share, text = "7")
+            IconWithText(iconRes = R.drawable.ic_comment, text = "8")
+            IconWithText(iconRes = R.drawable.ic_like, text = "23")
+        }
+    }
+}
+
+@Composable
+fun IconWithText(
+    @DrawableRes iconRes: Int,
+    text: String
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(id = iconRes),
+            contentDescription = text,
+            tint = MaterialTheme.colors.onSecondary
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = text, color = MaterialTheme.colors.onSecondary)
     }
 }
 
