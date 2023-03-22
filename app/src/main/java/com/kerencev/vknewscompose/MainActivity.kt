4a1c494976f30de97a362c3fad456ad0571e066e
Sergei Kerencev <SYKerentsev@sberbank.ru>
2023-03-22 13:17:18 +0300

    Added a news card header

--- app/src/main/java/com/kerencev/vknewscompose/MainActivity.kt
+++ app/src/main/java/com/kerencev/vknewscompose/MainActivity.kt
@@ -3,13 +3,23 @@ package com.kerencev.vknewscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kerencev.vknewscompose.ui.theme.VkNewsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,68 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                NewsCard()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(50.dp),
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = stringResource(
+                    id = R.string.group_avatar
+                )
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 4.dp)
+            ) {
+                Text(
+                    text = "Название группы",
+                    color = MaterialTheme.colors.onPrimary
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = "14:00",
+                    color = MaterialTheme.colors.onSecondary
+                )
+            }
+            Icon(
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = stringResource(id = R.string.more),
+                tint = MaterialTheme.colors.onSecondary
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    VkNewsComposeTheme {
-        Greeting("Android")
+fun NewsCardPreviewLight() {
+    VkNewsComposeTheme(darkTheme = false) {
+        NewsCard()
+    }
+}
+
+@Preview
+@Composable
+fun NewsCardPreviewNight() {
+    VkNewsComposeTheme(darkTheme = true) {
+        NewsCard()
     }
 }
