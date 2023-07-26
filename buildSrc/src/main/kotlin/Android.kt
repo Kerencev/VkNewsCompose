75a1296750177b9e57e596c9d8848304e89cff71
Sergei Kerencev <Kerentzev@bk.ru>
2023-07-26 15:43:12 +0300

    Gradle groovy to Kotlin Dsl

---
+++ buildSrc/src/main/kotlin/Android.kt
@@ -0,0 +1,9 @@
+object Android {
+
+    const val APPLICATION_ID = "com.kerencev.vknewscompose"
+    const val TARGET_SDK = 33
+    const val MIN_SDK = 23
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+    const val JVM_TARGET = "1.8"
+}
