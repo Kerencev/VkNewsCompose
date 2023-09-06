7d55977dfa91a96f776e67cb31cfea645716ae84
Kerentsev Sergei <Kerentzev@bk.ru>
2023-09-06 19:42:02 +0300

    Added documentation for repositories

--- app/src/main/java/com/kerencev/vknewscompose/domain/repositories/FriendsRepository.kt
+++ app/src/main/java/com/kerencev/vknewscompose/domain/repositories/FriendsRepository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 interface FriendsRepository {
 
     /**
-     * Get list of users's friends
+     * Get list of users friends
      * @param userId - Id of the user for whom the friends list will be uploaded
      * @param searchText - The text by which the list of friends will be received
      * @param isRefresh - The list of friends is updated or added
--- app/src/main/java/com/kerencev/vknewscompose/domain/repositories/SuggestedRepository.kt
+++ app/src/main/java/com/kerencev/vknewscompose/domain/repositories/SuggestedRepository.kt
@@ -5,5 +5,8 @@ import kotlinx.coroutines.flow.Flow
 
 interface SuggestedRepository {
 
+    /**
+     * Get list of users, groups and communities recommended for the user
+     */
     fun getSuggested(): Flow<List<Profile>>
 }
