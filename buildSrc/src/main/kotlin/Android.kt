9f01d0f858ab541d97083fd8078daac965594526
Sergei Kerentsev <Kerentzev@bk.ru>
2023-09-18 18:02:24 +0300

    Min Android SDK raised to 27 because of specifics of compose and VK SDK

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,7 +2,7 @@ object Android {
 
     const val APPLICATION_ID = "com.kerencev.vknewscompose"
     const val TARGET_SDK = 34
-    const val MIN_SDK = 23
+    const val MIN_SDK = 27
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
     const val JVM_TARGET = "1.8"
